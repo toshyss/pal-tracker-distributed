9342b45baea0c884079394b8bd82a656f5bfb022
toshyss <me@exmample.com>
2019-10-21 17:10:48 +0900

    modified somefile

--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,3 +1,42 @@
+//package io.pivotal.pal.tracker.timesheets;
+//
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.web.client.RestOperations;
+//
+//import java.util.IdentityHashMap;
+//import java.util.Map;
+//import java.util.concurrent.ConcurrentHashMap;
+//
+//public class ProjectClient {
+//
+//    private final RestOperations restOperations;
+//    private final String endpoint;
+//    private Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
+//    private final Logger logger = LoggerFactory.getLogger(getClass());
+//
+//    public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
+//        this.restOperations = restOperations;
+//        this.endpoint = registrationServerEndpoint;
+//    }
+//
+//    @HystrixCommand(fallbackMethod = "getProjectFromCache")
+//    public ProjectInfo getProject(long projectId) {
+////        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+//        ProjectInfo project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+//
+//        projectsCache.put(projectId, project);
+//
+//        return project;
+//    }
+//
+//    public ProjectInfo getProjectFromCache(long projectId) {
+//        logger.info("Getting project with id {} from cache", projectId);
+//
+//        return projectsCache.get(projectId);
+//    }
+//}
 package io.pivotal.pal.tracker.timesheets;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
@@ -5,16 +44,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
 
-import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
     private final RestOperations restOperations;
     private final String endpoint;
-    private Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
-    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations = restOperations;
@@ -23,7 +61,6 @@ public class ProjectClient {
 
     @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-//        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
         ProjectInfo project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
 
         projectsCache.put(projectId, project);
@@ -33,7 +70,6 @@ public class ProjectClient {
 
     public ProjectInfo getProjectFromCache(long projectId) {
         logger.info("Getting project with id {} from cache", projectId);
-
         return projectsCache.get(projectId);
     }
 }
